5f171b5c2625caf1daebefa371fcf63cdfd72827
Tuva Gunnarsson <tuva.mt.gunnarsson@gmail.com>
2025-06-18 10:07:15 +0200

    info

--- app.py
+++ app.py
@@ -72,7 +72,6 @@ def show_stage_1():
     st.info(
         """
         **Instructions:**
-            Lycka till
 
             Select a standard portfolio or create your own by choosing assets.
 
